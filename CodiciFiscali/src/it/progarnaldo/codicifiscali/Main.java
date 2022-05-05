b9c803bfc40e28596f5fe24f8d4373f4debf09fc
ErjonaMaxhaku <erjonamaxhaku@gmail.com>
2022-05-05 11:40:47 +0200

    aggiunta xml comuni e codici fiscali

--- CodiciFiscali/src/it/progarnaldo/codicifiscali/Main.java
+++ CodiciFiscali/src/it/progarnaldo/codicifiscali/Main.java
@@ -1,10 +1,9 @@
 package it.progarnaldo.codicifiscali;
 
 public class Main {
-	public static final String PEOPLE = "C:\\Users\\Erjona\\OneDrive\\Documents\\JAVA";
-	public static final String PP ="src\\it\\progarnaldo\\codiciFiscali\\inputPersone.xml"
-	public static final String MUNICIPALITY = "C:\\Users\\Erjona\\OneDrive\\Documents\\JAVA";
-	public static final String CODE = "C:\\Users\\Erjona\\OneDrive\\Documents\\JAVA";
+	public static final String PEOPLE = "src\\it\\progarnaldo\\codiciFiscali\\inputPersone.xml";
+	public static final String MUNICIPALITY = "src\\it\\progarnaldo\\codiciFiscali\\comuni.xml";
+	public static final String CODE = "src\\it\\progarnaldo\\codiciFiscali\\codiciFiscali.xml";
 
 	public static void main(String[] args) {
 	
